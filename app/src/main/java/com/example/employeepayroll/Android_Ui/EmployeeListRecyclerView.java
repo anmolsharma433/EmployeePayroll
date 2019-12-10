2f388fb51b71700853dd6a64540125f6feef94a2
anmol sharma <anmolsharma433@gmail.com>
2019-12-09 22:14:05 -0500

    Started work on recycler view

---
+++ app/src/main/java/com/example/employeepayroll/Android_Ui/EmployeeListRecyclerView.java
@@ -0,0 +1,16 @@
+package com.example.employeepayroll.Android_Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.employeepayroll.R;
+
+public class EmployeeListRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_list_recycler_view);
+    }
+}
