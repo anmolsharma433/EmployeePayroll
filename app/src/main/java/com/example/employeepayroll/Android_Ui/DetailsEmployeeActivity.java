822917e1eb010b84aed980623777d98b325a5b9c
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2019-12-10 16:30:25 -0500

    new activity for employee details

---
+++ app/src/main/java/com/example/employeepayroll/Android_Ui/DetailsEmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.example.employeepayroll.Android_Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.employeepayroll.R;
+
+public class DetailsEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_employee);
+    }
+}
