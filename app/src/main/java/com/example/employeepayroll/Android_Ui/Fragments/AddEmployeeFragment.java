fdf8f06a0277c24e515de05e99cc178ddf703a10
anmol sharma <anmolsharma433@gmail.com>
2019-12-10 00:40:29 -0500

    launcher icon updates add Employee Done

--- app/src/main/java/com/example/employeepayroll/Android_Ui/Fragments/AddEmployeeFragment.java
+++ app/src/main/java/com/example/employeepayroll/Android_Ui/Fragments/AddEmployeeFragment.java
@@ -15,6 +15,6 @@ public class AddEmployeeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_employee,container,false);
+        return inflater.inflate(R.layout.fragment_add_employee,container,false);
     }
 }
